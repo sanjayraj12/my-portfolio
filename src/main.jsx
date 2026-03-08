6892b1b4c977afa57ce7efde53c19bf67c63671f
sanjayraj12 <jsanjayraj012@gmail.com>
2026-03-08 22:49:23 +0530

    Add GTM integration

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import TagManager from 'react-gtm-module' // Import the library
+
+// Initialize GTM with your ID
+const tagManagerArgs = {
+    gtmId: 'GTM-M98ZFRXG'
+};
+
+TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
